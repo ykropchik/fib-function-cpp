3a5cd9feb7972fe9bd9909a78ab198bad5c36f7d
YKROPCHIK <ykropchik22@gmail.com>
2019-10-12 13:40:10 +1000

    Create fib function

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "fiblibrary.h"
+
+using namespace std;
+
+int fib (int n){
+    if ((n == 1) || (n == 2)){
+        return 1;
+    } else {
+        return fib(n - 1) + fib(n - 2);
+    }
+}
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    cout << fib(n);
+}
